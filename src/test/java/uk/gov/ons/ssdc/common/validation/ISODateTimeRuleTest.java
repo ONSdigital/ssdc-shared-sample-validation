5eeb764fd6b7e308952cf4b2ab2cd136e4d15e39
Hugh Brace <hugh.ons@gmail.com>
2021-10-14 07:56:11 +0100

    Add validators for date (time)

---
+++ src/test/java/uk/gov/ons/ssdc/common/validation/ISODateTimeRuleTest.java
@@ -0,0 +1,24 @@
+package uk.gov.ons.ssdc.common.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+
+public class ISODateTimeRuleTest {
+
+  @Test
+  void checkValidityValid() {
+    ISODateTimeRule underTest = new ISODateTimeRule();
+    Optional<String> validity = underTest.checkValidity("2021-10-09T15:45:33.123Z");
+    assertThat(validity.isPresent()).isFalse();
+  }
+
+  @Test
+  void checkValidityInvalid() {
+    ISODateTimeRule underTest = new ISODateTimeRule();
+    Optional<String> validity = underTest.checkValidity("66-66-6666:66:66:66.666Z");
+    assertThat(validity.isPresent()).isTrue();
+    assertThat(validity.get()).isEqualTo("Not a valid ISO date time");
+  }
+}
