c2a9d2aeea2e683756425aa7bdc89192e3f75a95
Kacper Prywata <kacper.prywata@ons.gov.uk>
2023-07-13 14:25:18 +0100

    Add test for non-ascii and non printable ascii

--- src/test/java/uk/gov/ons/ssdc/common/validation/RegexRuleTest.java
+++ src/test/java/uk/gov/ons/ssdc/common/validation/RegexRuleTest.java
@@ -9,6 +9,10 @@ class RegexRuleTest {
   private static final String TEST_REGEX_UK_MOBILE_NUMBER = "^07[0-9]{9}$";
   private static final String TEST_REGEX_UK_MOBILE_NUMBER_ERROR = "Not a valid UK mobile number";
 
+  private static final String TEST_REGEX_PRINTABE_ASCII_ONLY = "[ -~]*";
+  private static final String TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR =
+      "Email contains non-ASCII characters";
+
   @Test
   void checkValidityValidPhoneNumber() {
     RegexRule underTest =
@@ -43,5 +47,35 @@ class RegexRuleTest {
 
     Optional<String> actualResult = underTest.checkValidity("07123456789123456789");
     assertThat(actualResult).isPresent().contains(TEST_REGEX_UK_MOBILE_NUMBER_ERROR);
+  }
+
+  @Test
+  void checkValidityValidEmailContainsPrintableAsciiOnly() {
+    RegexRule underTest =
+        new RegexRule(TEST_REGEX_PRINTABE_ASCII_ONLY, TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("valid@mail.com");
+    assertThat(actualResult).isNotPresent();
+  }
+
+  @Test
+  void checkValidityInvalidEmailContainsNonAscii() {
+    RegexRule underTest =
+        new RegexRule(TEST_REGEX_PRINTABE_ASCII_ONLY, TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity("\u00F6invalid@mail.com");
+    assertThat(actualResult).isPresent().contains(TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
+  }
+
+  @Test
+  void checkValidityInvalidEmailContainsNonPrintableAscii() {
+    RegexRule underTest =
+        new RegexRule(TEST_REGEX_PRINTABE_ASCII_ONLY, TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
+
+    Optional<String> actualResult = underTest.checkValidity((char) 31 + "invalid@mail.com");
+    assertThat(actualResult).isPresent().contains(TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
+
+    actualResult = underTest.checkValidity((char) 127 + "invalid@mail.com");
+    assertThat(actualResult).isPresent().contains(TEST_REGEX_PRINTABE_ASCII_ONLY_ERROR);
   }
 }
